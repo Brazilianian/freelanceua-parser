b4afcc9348e9193a46785e7b72341ed900469ddc
Vitalii <vetajika@gmail.com>
2023-06-07 17:16:58 +0300

    (fix) find by name in freelance_site_repository.py

--- repository/freelance_site_repository.py
+++ repository/freelance_site_repository.py
@@ -2,8 +2,9 @@ from model.freelance_site_model import FreelanceSiteModel
 
 
 def find_by_name(name: str):
-    sites = FreelanceSiteModel.select().where(FreelanceSiteModel.name == name).limit(1)
-    return sites[0] if len(sites) > 0 else sites
+    query = FreelanceSiteModel.select().where(FreelanceSiteModel.name == name).limit(1)
+    results = query.execute()
+    return results[0] if len(results) > 1 else results
 
 
 def find_by_id(site_id: int):
