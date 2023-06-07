204a5c83726b94df41ff385776bb2bcb67d86c12
Vitalii <vetajika@gmail.com>
2023-06-07 16:51:49 +0300

    (fix) find by name in freelance_site_repository.py

--- repository/freelance_site_repository.py
+++ repository/freelance_site_repository.py
@@ -2,7 +2,7 @@ from model.freelance_site_model import FreelanceSiteModel
 
 
 def find_by_name(name: str):
-    return FreelanceSiteModel.get(name=name)
+    return FreelanceSiteModel.get(FreelanceSiteModel.name == name)
     pass
 
 
